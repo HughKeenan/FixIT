28c6fae0bb5499e869d53b52578bcf00734cb7d3
Hugh Keenan <hugh.keenan114@gmail.com>
2025-05-18 15:02:38 +0000

    create contact page

--- fixit_app/views.py
+++ fixit_app/views.py
@@ -227,6 +227,15 @@ def about(request):
 
     return render(request, 'about.html', context)
 
+def contact(request):
+    context = {'year': datetime.now().year}
+
+    if request.user.is_authenticated:
+        context['last_question'] = request.session.pop('last_question', None)
+        context['last_response'] = request.session.pop('last_response', None)
+
+    return render(request, 'contact.html', context)
+
 def meet_the_team(request):
     context = {
         'title': 'Meet the Team',  # Example data
